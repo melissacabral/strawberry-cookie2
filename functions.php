8ddde2c0e5ad15074a3bef87774566e9b8ee10ad
Melissa Cabral <melissacabral@gmail.com>
2020-05-15 08:01:29 -0700

    added woocommerce support

--- functions.php
+++ functions.php
@@ -23,6 +23,7 @@ add_theme_support( 'automatic-feed-links' );
 $args = array(
 	'width' => 1000,
 	'height' => 600,	
+	'default-image' => get_template_directory_uri() . '/images/header-bg.png',
 );
 add_theme_support( 'custom-header', $args );
 
@@ -32,6 +33,7 @@ $args = array(
 	'height' 	=> 300,
 	'flex-width' => true,
 	'flex-height'=> true,
+	'header-text' => array( 'site-title', 'site-description' ),
 );
 add_theme_support( 'custom-logo', $args );
 
@@ -75,9 +77,13 @@ add_action( 'init', 'sc_menu_setup' );
 
 //attach any needed stylesheets
 function sc_styles(){
+	
 	//genericons for social menu
 	$url = get_stylesheet_directory_uri() . '/genericons/genericons.css';
 	wp_enqueue_style( 'genericons', $url );
+
+	//style.css
+	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'sc_styles' );
 
@@ -105,6 +111,14 @@ function sc_widget_areas(){
 		'name'	=> 'Page Aside',
 		'id' => 'page-aside',
 		'description' => 'Displays next to static pages',
+		'before_widget' => '<section class="widget %2$s" id="%1$s">',
+		'after_widget' => '</section>',
+	) );
+	
+	register_sidebar( array(
+		'name'	=> 'Shop Sidebar',
+		'id' => 'shop-aside',
+		'description' => 'Displays next to WooCommerce shop pages',
 		'before_widget' => '<section class="widget %2$s" id="%1$s">',
 		'after_widget' => '</section>',
 	) );
@@ -145,6 +159,56 @@ function sc_pings_count(){
 	}
 
 	echo $count == 1 ? __('One Site Links Here', 'strawberry-cookie' ) : $count . __(' Sites Link Here', 'strawberry-cookie');
+}
+
+
+/*woocommerce additions*/
+add_action( 'after_setup_theme', 'sc_woo' );
+function sc_woo(){
+	add_theme_support( 'woocommerce' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
+}
+
+
+/**
+ * Woocommerce Show cart contents / total Ajax
+ * https://docs.woocommerce.com/document/show-cart-contents-total/
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
+
+
+//disable all woocommerce styles
+//add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+// make woocommerce work with our page structure
+ //remove the default content wrapper HTML
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+//add our correct content wrapper HTML
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<main class="content">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</main>';
 }
 
 
